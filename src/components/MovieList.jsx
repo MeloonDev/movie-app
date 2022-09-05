93e418b41561751e38a1f435bbd1e4d21f56dd14
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2022-09-05 22:43:07 +0200

    Add Hover Effect

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,17 @@
-const MovieList = ({ movies }) => {
+const MovieList = ({ movies, favouriteComponent }) => {
+  const FavouriteComponent = favouriteComponent;
+
   return (
     <>
       {movies.map((movie, index) => (
-        <div className="d-flex justify-content-start m-3" key={index}>
+        <div
+          className="image-container d-flex justify-content-start m-3"
+          key={index}
+        >
           <img src={movie.Poster} alt={movie.Title} />
+          <div className="overlay d-flex align-items-center justify-content-center">
+            <FavouriteComponent />
+          </div>
         </div>
       ))}
     </>
